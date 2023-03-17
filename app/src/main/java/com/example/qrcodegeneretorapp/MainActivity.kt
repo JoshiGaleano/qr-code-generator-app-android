59a78aa09ad92eb7f968b16477bf7715bbc31df1
Josimar Galeano <ing.galeano24@gmail.com>
2023-03-17 07:11:30 -0500

    Implement a qr code generator

---
+++ app/src/main/java/com/example/qrcodegeneretorapp/MainActivity.kt
@@ -0,0 +1,121 @@
+package com.example.qrcodegeneretorapp
+
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.qrcodegeneretorapp.ui.theme.QrCodeGeneretorAppTheme
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.EncodeHintType
+import com.google.zxing.WriterException
+import com.google.zxing.qrcode.QRCodeWriter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            QrCodeGeneretorAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    RenderQr()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun RenderQr() {
+    Image(
+        painter = rememberQrBitmapPainter(content = "https://dev.to"),
+        contentDescription = "DEV Communit Code",
+        contentScale = ContentScale.FillBounds,
+        modifier = Modifier.size(135.dp),
+    )
+}
+
+@Composable
+fun rememberQrBitmapPainter(
+    content: String,
+    size: Dp = 150.dp,
+    padding: Dp = 0.dp
+): BitmapPainter {
+
+    val density = LocalDensity.current
+    val sizePx = with(density) { size.roundToPx() }
+    val paddingPx = with(density) { padding.roundToPx() }
+
+    var bitmap by remember(content) {
+        mutableStateOf<Bitmap?>(null)
+    }
+
+    LaunchedEffect(bitmap) {
+        if (bitmap != null) return@LaunchedEffect
+
+        launch(Dispatchers.IO) {
+            val qrCodeWriter = QRCodeWriter()
+
+            val encodeHints = mutableMapOf<EncodeHintType, Any?>()
+                .apply {
+                    this[EncodeHintType.MARGIN] = paddingPx
+                }
+
+            val bitmapMatrix = try {
+                qrCodeWriter.encode(
+                    content, BarcodeFormat.QR_CODE,
+                    sizePx, sizePx, encodeHints
+                )
+            } catch (ex: WriterException) {
+                null
+            }
+
+            val matrixWidth = bitmapMatrix?.width ?: sizePx
+            val matrixHeight = bitmapMatrix?.height ?: sizePx
+
+            val newBitmap = Bitmap.createBitmap(
+                bitmapMatrix?.width ?: sizePx,
+                bitmapMatrix?.height ?: sizePx,
+                Bitmap.Config.ARGB_8888
+            )
+
+            for (x in 0 until matrixWidth) {
+                for (y in 0 until matrixHeight) {
+                    val shouldColorPixel = bitmapMatrix?.get(x, y) ?: false
+                    val pixelColor = if (shouldColorPixel) Color.BLACK else Color.WHITE
+
+                    newBitmap.setPixel(x, y, pixelColor)
+                }
+            }
+
+            bitmap = newBitmap
+        }
+    }
+
+    return remember(bitmap) {
+        val currentBitmap = bitmap ?: Bitmap.createBitmap(
+            sizePx, sizePx,
+            Bitmap.Config.ARGB_8888,
+        ).apply { eraseColor(Color.TRANSPARENT) }
+
+        BitmapPainter(currentBitmap.asImageBitmap())
+    }
+}
